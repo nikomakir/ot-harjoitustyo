c373a1af7258ae1a8d6d688e6b5b7ac135db6e00
Niko Raulas <niko.raulas@outlook.com>
2023-05-13 14:36:09 +0300

    lisätty testejä

---
+++ src/tests/difficulty_repository_test.py
@@ -0,0 +1,16 @@
+import unittest
+from repositories.difficulty_repository import difficulty_repository
+
+
+class TestDifficultyRepository(unittest.TestCase):
+    def setUp(self):
+        difficulty_repository.write(0)
+        self.difficulty = 50
+
+    def test_write(self):
+        difficulty_repository.write(self.difficulty)
+        self.assertEqual(self.difficulty, difficulty_repository.load())
+
+    def test_load(self):
+        self.assertEqual(difficulty_repository.load(), 0)
+            
